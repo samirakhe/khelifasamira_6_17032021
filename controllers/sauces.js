f200d38ab07f2f132cf4070e59d70c3cb8f664f9
sam khe <visuelkoncept@gmail.com>
2021-09-06 12:03:54 +0200

    debutp6

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,40 +1,36 @@
+/* eslint-disable linebreak-style */
 const Sauce = require('../models/sauces');
-const queryString =  require('querystring');
 
+exports.createSauce = (req, res) => {
+  // delete req.body._id;
+  const sauce = new Sauce({
+    ...req.body,
+  });
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
-exports.createSauce =  (req, res, next) => {
-    delete req.body._id;
-    
-    //console.log(req);
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  };
+exports.modifySauce = (req, res) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
-  exports.modifySauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  };
+exports.deleteSauce = (req, res) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
-  exports.deleteSauce =  (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  };
+exports.getOneSauce = (req, res) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
 
-  
-  exports.getOneSauce =  (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(404).json({ error }));
-  };
-
-  exports.getAllSauce = (req, res, next) => {
-    Sauce.find()
-      .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({ error }));
-  };
+exports.getAllSauce = (req, res) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
